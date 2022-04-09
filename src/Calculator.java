748f38d1faa8688784540fdd5afe0b3b9bb40d32
Sergül Sopcalı <74403964+sergull@users.noreply.github.com>
2022-04-09 16:43:10 +0300

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,17 +1,31 @@
 import java.util.Scanner;
-
 public class Calculator {
-
     public static void main(String[] args) {
         double num1;
         double num2;
+        double ans;
+        char op;
         Scanner reader = new Scanner(System.in);
         System.out.print("Enter two numbers: ");
         num1 = reader.nextDouble();
         num2 = reader.nextDouble();
+        System.out.print("\nEnter an operator (+, -, *, /): ");
+        op = reader.next().charAt(0);
+        if(op=='+'){
+            System.out.println(num1+num2);
+        }
 
+        else if(op=='-'){
+            System.out.println(num1-num2);
+        }
 
-        System.out.println( num1 + num2);
-
+        else if(op=='*'){
+            System.out.println(num1*num2);
+        }
+        else if(op=='/'){
+            if (num2!=0){
+                System.out.println(num1/num2);
+            }
+        }
     }
 }
